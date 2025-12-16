d93eb4fc11acadb149d5432434d55c8949dfe651
Xavier Mitault <xavier.mitault@epitech.eu>
2025-12-16 12:41:14 +0100

    Add default url for profile picture

--- backend/epitrello/myauth/models.py
+++ backend/epitrello/myauth/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -10,7 +11,7 @@ AUTH_PROVIDERS = [
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=30)
-    profile_picture = models.URLField()
+    profile_picture = models.URLField(default=f"{settings.STATIC_URL}question_mark.png")
     email = models.EmailField()
     authprovider = models.CharField(max_length=3, choices=AUTH_PROVIDERS)
     authuserid = models.CharField(max_length=30)
