d7ecd8435614d982d88cc7b44472ac7521bdc31b
adelaide <adelaide.cahnteux@epitech.eu>
2025-12-29 16:49:48 +0100

    Add all route in bridge

--- frontend/app/composables/service/bridge.ts
+++ frontend/app/composables/service/bridge.ts
@@ -35,4 +35,140 @@ export class bridge {
             throw err;
         }
     }
+
+    async getBoards(userID: string) {
+        try {
+            const response = await fetch(this.url + `/get/board/` + userID + `/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+            });
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / fetch boards error', err);
+            throw err;
+        }
+    }
+
+    async createBoard(boardName: string) {
+        try {
+            const response = await fetch(this.url + `/create/board/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'appliaction/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+                body: JSON.stringify(boardName)
+            });
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / create board error', err);
+            throw err;
+        }
+    }
+
+    async deleteBoard(boardID: string) {
+        try {
+            const response = await fetch(this.url + `/delete/board/` + boardID, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'appliaction/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+            });
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / delete board error', err);
+            throw err;
+        }
+    }
+
+    async createTask(boardID: string, data: any) {
+        try {
+            const response = await fetch(this.url + `/create/task/` + boardID + `/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'appliaction/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+                body: JSON.stringify(data)
+            });
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / create task error', err);
+            throw err;
+        }
+    }
+
+    async deleteTask(boardID: string, taskID: string) {
+        try {
+            const response = await fetch(this.url + `/delete/task/` + boardID + `/` + taskID + `/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'appliaction/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+            })
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / delete task error', err);
+            throw err;
+        }
+    }
+
+    async deleteArchive(boardID: string, taskID: string) {
+        try {
+            const response = await fetch(this.url + `/deleteforce/task/` + boardID + `/` + taskID + `/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'appliaction/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+            })
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / delete task error', err);
+            throw err;
+        }
+    }
+
+    async updateTask(taskID: string, data: any) {
+        try {
+            const response = await fetch(this.url + `/update/task/` + taskID + `/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'appliaction/json',
+                    'Authorization': 'Bearer' + this.jwt
+                },
+                body: JSON.stringify(data)
+            })
+            if (response.ok) {
+                return { status: response.status, success: true };
+            }
+            throw { status: response.status, success: false };
+        } catch (err) {
+            console.error('Network / delete task error', err);
+            throw err;
+        }
+    }
 }
