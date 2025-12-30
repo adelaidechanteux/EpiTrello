9f89458e0296c27b1f99776f110206188970e715
Xavier Mitault <xavier.mitault@epitech.eu>
2025-12-30 12:05:07 +0100

    Add typing

--- backend/epitrello/myauth/models.py
+++ backend/epitrello/myauth/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.conf import settings
+from typing import final
 
 # Create your models here.
 
@@ -9,10 +10,11 @@ AUTH_PROVIDERS = [
 ]
 
 
+@final
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=30)
-    profile_picture = models.URLField(default=f"{settings.STATIC_URL}question_mark.png")
-    email = models.EmailField()
-    authprovider = models.CharField(max_length=3, choices=AUTH_PROVIDERS)
-    authuserid = models.CharField(max_length=30)
+    id = models.UUIDField[uuid.UUID, uuid.UUID](primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField[str, str](max_length=30)
+    profile_picture = models.URLField[str, str](default=f"{settings.STATIC_URL}question_mark.png")
+    email = models.EmailField[str, str]()
+    authprovider = models.CharField[str, str](max_length=3, choices=AUTH_PROVIDERS)
+    authuserid = models.CharField[str, str](max_length=30)
