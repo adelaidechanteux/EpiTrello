4b825294ac2c4ec1f558b5e42213a8053a6664e7
Xavier Mitault <xavier.mitault@epitech.eu>
2026-01-27 21:34:00 +0100

    Use proxy backend for frontend

--- frontend/app/pages/boards/[boardID].vue
+++ frontend/app/pages/boards/[boardID].vue
@@ -153,7 +153,8 @@ const boardID = computed(() => {
 
 const { $bridge } = useNuxtApp()
 const api = $bridge
-const ws = useBoardSocket(boardID.value!)
+const requestUrl = useRequestURL()
+const ws = useBoardSocket(boardID.value!, `${requestUrl.hostname}:${requestUrl.port}`)
 const auth = useAuthStore()
 
 const board = ref<Column[]>([])
